b5259df9081d02f0824d05efe4a3745f9b29d403
ryantstone <ryan@ryanstone.io>
2019-02-16 08:17:42 -0500

    Initial Commit

---
+++ Squish/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Squish
+//
+//  Created by Sharkmaul on 2/16/19.
+//  Copyright © 2019 Sharkmaul. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Squish/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Squish
+//
+//  Created by Sharkmaul on 2/16/19.
+//  Copyright © 2019 Sharkmaul. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
