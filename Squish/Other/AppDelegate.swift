406e0a7381157b4be75bde375454a1a6e0aa8d2a
ryantstone <ryan@ryanstone.io>
2019-02-16 12:30:55 -0500

    wip

--- Squish/Other/AppDelegate.swift
+++ Squish/Other/AppDelegate.swift
@@ -1,26 +1,12 @@
-//
-//  AppDelegate.swift
-//  Squish
-//
-//  Created by Sharkmaul on 2/16/19.
-//  Copyright © 2019 Sharkmaul. All rights reserved.
-//
-
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
 }
-
---
+++ Squish/Views/FileList/FileListViewController.swift
@@ -0,0 +1,7 @@
+import AppKit
+
+public final class FileListViewController: NSViewController {
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
